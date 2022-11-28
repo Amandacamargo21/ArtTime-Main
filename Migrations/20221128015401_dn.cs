c09db449dad83dcd9649f880082e130ca0b2c476
Gagliassi <gagliassijoao@gmail.com>
2022-11-27 22:56:10 -0300

    correção data

---
+++ Migrations/20221128015401_dn.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ArtTime.Migrations
+{
+    public partial class dn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "dataNascimento",
+                table: "Clientes",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "dataNascimento",
+                table: "Clientes");
+        }
+    }
+}
--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -12,6 +12,7 @@ namespace ArtTime.Models
         public int clienteId { get; set; }
         public string Nome { get; set; }
         public string cpf { get; set; }
+        public string dataNascimento { get; set; }
         public DateTime CriadoEm { get; set; }
 
     }
